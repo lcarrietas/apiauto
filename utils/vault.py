30696cacd9faa2ca8bef5877ca5d63dbf1cfe923
Luis Carlos Arrieta Sierra <lcarrietas@unal.edu.co>
2023-09-25 18:22:51 -0500

    Update vault.py

--- utils/vault.py
+++ utils/vault.py
@@ -1,7 +1,9 @@
 import hvac
 
-
 def get_token():
+    """
+    Get API Token from Vault
+    """
     client = hvac.Client(url="http://127.0.0.1:8200/", token="dev-only-token")
 
     read_response = client.secrets.kv.read_secret_version(path="path-todo-token")
