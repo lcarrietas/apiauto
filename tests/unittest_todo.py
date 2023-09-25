9e538d8f1e3e42e40c2cc92c69f2107cefd461ae
Luis Carlos Arrieta Sierra <lcarrietas@unal.edu.co>
2023-09-25 18:20:35 -0500

    Update unittest_todo.py

--- tests/unittest_todo.py
+++ tests/unittest_todo.py
@@ -5,12 +5,21 @@ class TestProject(unittest.TestCase):
 
     # fixture
     def setUp(self):
+         """
+        Test Setup
+        """
         print("Setup")
 
     def test_one(self):
+         """
+        Test one
+        """
         print("test one")
 
     def test_two(self):
+         """
+        Test two
+        """
         print("test two")
 
     def test_three(self):
@@ -18,4 +27,7 @@ class TestProject(unittest.TestCase):
 
     # fixture
     def tearDown(self):
+         """
+        Test three
+        """
         print("tearDown")
